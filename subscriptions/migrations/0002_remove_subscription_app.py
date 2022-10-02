af3545ea3603ec3efa144c3bf563c6083f353c81
Crowdbotics <team@crowdbotics.com>
2022-10-02 06:13:31 +0000

    Update models

--- subscriptions/migrations/0002_remove_subscription_app.py
+++ subscriptions/migrations/0002_remove_subscription_app.py
@@ -6,12 +6,12 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('subscriptions', '0001_initial'),
+        ("subscriptions", "0001_initial"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='subscription',
-            name='app',
+            model_name="subscription",
+            name="app",
         ),
     ]
